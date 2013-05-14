3ef0a4f89df65dccb1ea549fb5842dc39512e592
毅 谭 <tanyi@takeshitekiMacBook-Pro.local>
2013-05-14 10:28:25 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author: tanyi@wandoujia.com
+# Created Time:05/14/13 10:27:25
+if [ -d ~/.vim ];then
+    rm -rf ~/.vim.bak
+    mv ~/.vim ~/.vim.bak
+fi
+if [ -f ~/.vimrc ];then
+    rm -f ~/.vimrc.bak
+    mv ~/.vimrc ~/.vimrc.bak
+fi
+cp -rf .vim ~/
+cp .vimrc ~/        
