e01538e24518dd8667309cd8a9eff0b8e3734650
毅 谭 <tanyi@takeshitekiMacBook-Pro.local>
2013-05-14 11:32:36 +0800

    add submoudle

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Author: tanyi@wandoujia.com
 # Created Time:05/14/13 10:27:25
+git submodule init
+git submodule update
 if [ -d ~/.vim ];then
     rm -rf ~/.vim.bak
     mv ~/.vim ~/.vim.bak
